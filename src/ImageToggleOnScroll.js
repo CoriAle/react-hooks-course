8d2b55e371a7963db0d16b08c3972581206bc34e
corina.nimatuj <corina.nimatuj@backcountry.com>
2020-12-29 17:55:38 -0600

    Getting demo code - Before refactor

--- src/ImageToggleOnScroll.js
+++ src/ImageToggleOnScroll.js
@@ -1,42 +1,45 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-const ImageToggleOnScroll = ({primaryImg, secondaryImg}) => {
-	const imageRef = useRef(null);
-	const [inView, setInView] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
+const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
+  const imageRef = useRef(null);
 
-	const isInView = () => {
-		const rect = imageRef.current.getBoundingClientRect();
-		return rect.top >= 0 && rect.bottom <= window.innerHeight;
-	};
+  const [isLoading, setIsLoading] = useState(true);
 
- 	useEffect(()  => {
- 		setIsLoading(false);
- 		setInView(isInView());
-		window.addEventListener("scroll", scrollHandler);
-		return () => {
-			window.removeEventListener("scroll", scrollHandler);
-		}; // This happens on component will unmount
+  const isInView = () => {
+    const rect = imageRef.current.getBoundingClientRect();
+    return rect.top >= 0 && rect.bottom <= window.innerHeight;
+  };
 
-	}, []); // It only happens on component did mount.
+  const [inView, setInView] = useState(false);
 
-	const scrollHandler = () => {
-		setInView(isInView());
-	};
+  useEffect(() => {
+    setIsLoading(false);
+    setInView(isInView());
+    window.addEventListener('scroll', scrollHandler);
+    return () => {
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
 
-	return (
-		<div>
-			<img
-				src={ 
-					isLoading ? 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
-					: inView ? primaryImg: secondaryImg
-				}
-				alt=""
-				ref={imageRef}/>
-		</div>
-	)
-}
+  const scrollHandler = () => {
+    setInView(isInView());
+  };
+
+  return (
+    <img
+      src={
+        isLoading
+          ? 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==' // 1x1gif
+          : inView
+          ? secondaryImg
+          : primaryImg
+      }
+      alt=""
+      ref={imageRef}
+      width="200"
+      height="200"
+    />
+  );
+};
 
 export default ImageToggleOnScroll;
-
-//imageRef.current.src = primaryImg;
